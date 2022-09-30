7e457632390029adaca933fd95a61754e217d41b
alabouchhima <108215643+alabouchhima@users.noreply.github.com>
2022-09-30 23:24:17 +0100

    Add files via upload

---
+++ trying.js
@@ -0,0 +1,12 @@
+function test(m){
+    switch(m){
+case "january" : 
+text = " winter ";
+break ;
+case "december" : 
+text = " summer";
+break ; 
+default : 
+text = " not a month";
+    }
+      }
